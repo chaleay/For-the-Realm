7cb1a556e53687cb5b4caa21d13ca3c3eb3ff77e
elijah gaytan <elijahgaytan@utexas.edu>
2018-12-27 00:41:00 -0600

    Started project, fitted workspace.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+    
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(LoadMainMenu());
+        }
+    }
+
+    public IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Start Screen");
+    }
+
+ 
+}
